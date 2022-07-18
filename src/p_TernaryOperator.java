899abe3a89c8d099da3467353de0aa50d3476978
Abdullah Fikri <abdullahfikrihandi@gmail.com>
2022-07-18 20:06:22 +0700

    Ternary Operator, Looping

---
+++ src/p_TernaryOperator.java
@@ -0,0 +1,23 @@
+public class p_TernaryOperator {
+    public static void main(String[] args) {
+
+        byte nilai = 75;
+        String ucapan;
+
+        // Dengan if-else
+
+        if (nilai >= 75) {
+            ucapan = "Selamat Anda Lulus";
+        } else {
+            ucapan = "Anda Tidak Lulus";
+        }
+
+        System.out.println(ucapan);
+
+        // Dengan ternary operator
+        nilai = 60;
+        String ucapan2 = nilai >= 75 ? "Selamat Anda Lulus" : "Anda Tidak Lulus"; // Depan TRUE, Belakang FALSE !!
+        System.out.println(ucapan2);
+
+    }
+}
---
+++ src/q_PerulanganFor.java
@@ -0,0 +1,5 @@
+public class q_PerulanganFor {
+    public static void main(String[] args) {
+        
+    }
+}
