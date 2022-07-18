da9a909922f063d0ddc4247502c7cd45036c11cd
Abdullah Fikri <abdullahfikrihandi@gmail.com>
2022-07-18 19:47:16 +0700

    Hola

--- src/a_HelloWorld.java
+++ src/a_HelloWorld.java
@@ -1,7 +1,7 @@
 public class a_HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World !");
         
     }
 }
