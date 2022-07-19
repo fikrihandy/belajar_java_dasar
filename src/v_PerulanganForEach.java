e5777b8faec369abaef39a7bffaac13e6f43c6a4
Abdullah Fikri <abdullahfikrihandi@gmail.com>
2022-07-19 15:32:37 +0700

    For Each Loop

---
+++ src/v_PerulanganForEach.java
@@ -0,0 +1,17 @@
+public class v_PerulanganForEach {
+    public static void main(String[] args) {
+        String[] array = {"Abdullah", "Fikri", "Handi", "Saputra"};
+
+        // Tanpa for each
+        System.out.println("1. Tanpa for each");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Array ke-" + i + " = " + array[i]);
+        }
+
+        // Dengan for each
+        System.out.println("\n2. Dengan for each");
+        for (var value : array) {
+            System.out.println(value);
+        }
+    }
+}
